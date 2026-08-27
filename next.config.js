718ddea628e4c485a84cba8b6d8238ce7ee6ff61
shabab966 <tshabab26@gmail.com>
2026-08-27 19:15:40 +0600

    fix(webpack): configure explicit webpack resolve alias for @ in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
   }
