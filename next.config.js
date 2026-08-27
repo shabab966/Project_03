bc44841261a8d60bd732137907c19c7991057acf
shabab966 <tshabab26@gmail.com>
2026-08-27 19:23:22 +0600

    fix: safely spread webpack resolve alias for @ and set baseUrl in tsconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
   }
